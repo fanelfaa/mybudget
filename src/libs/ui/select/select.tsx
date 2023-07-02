8cf8e486f73f0f88760c3fb7730dd8fdc15a9419
fandi <fandi@zero-one-group.com>
2023-07-02 21:03:54 +0700

    chore: delete select

--- src/libs/ui/select/select.tsx
+++
@@ -1,8 +0,0 @@
-import ReactSelect, { Props } from 'react-select';
-
-type Option = { value: string; label: string };
-export const Select = (props: Props<Option, false>) => {
-	return (
-		<ReactSelect<Option> isClearable={false} menuPlacement="top" {...props} />
-	);
-};
