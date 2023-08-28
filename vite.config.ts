c22cf70703ca038fea06ea87d14e54647db8e48e
Fandi Ilham <105334738+fanelfaa@users.noreply.github.com>
2023-08-29 05:40:33 +0700

    feat: add pwa (#2)
    * feat: add pwa
    
    * remove
    
    * chore: ignore dev-dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			devOptions: {
+				enabled: true,
+			},
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			manifest: {
+				name: 'Budget AF',
+				short_name: 'BudgetAF',
+				description: 'Budget your expenses',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
