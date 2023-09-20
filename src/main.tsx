24216b4d39adc565a3d524dc1dc0755b03be37c3
fandi <fandi@zero-one-group.com>
2023-09-20 19:14:09 +0700

    feat: add color mode script to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
+import { theme } from './libs/ui/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
+		<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 	</React.StrictMode>
 );
