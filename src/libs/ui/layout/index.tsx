c0a7f3da9d9db315d29176312087870f65416bf7
fandi <fandi@zero-one-group.com>
2023-09-20 19:32:45 +0700

    feat: add toggle theme

--- src/libs/ui/layout/index.tsx
+++ src/libs/ui/layout/index.tsx
@@ -1,21 +1,37 @@
-import { Box, BoxProps } from '@chakra-ui/react';
+import { Box, BoxProps, IconButton, useColorMode } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { FiSun, FiMoon } from 'react-icons/fi';
 
 export type BaseLayoutProps = BoxProps;
 
-export const BaseLayout = (props: BaseLayoutProps) => (
-	<Box
-		p="4"
-		pt="0"
-		shadow="md"
-		rounded="md"
-		minH="100vh"
-		w="md"
-		maxW="100%"
-		bgColor="Background"
-		mx="auto"
-		{...props}
-	>
-		<Outlet />
-	</Box>
-);
+export const BaseLayout = (props: BaseLayoutProps) => {
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	return (
+		<Box
+			p="4"
+			pt="0"
+			shadow="md"
+			rounded="md"
+			minH="100vh"
+			w="md"
+			maxW="100%"
+			bgColor="Background"
+			mx="auto"
+			{...props}
+		>
+			<Outlet />
+			<Box position="fixed" bottom="4" left="4">
+				<IconButton
+					borderWidth="1px"
+					borderColor="MBorder"
+					icon={colorMode === 'dark' ? <FiSun /> : <FiMoon />}
+					onClick={toggleColorMode}
+					aria-label="change theme"
+					title="Change Theme"
+					background="Background"
+				/>
+			</Box>
+		</Box>
+	);
+};
